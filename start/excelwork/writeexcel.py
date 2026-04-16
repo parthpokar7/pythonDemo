4fa3a866807293627662dd445efe4b03a902b5cf
parthpokar7 <parthpokar.77@gmail.com>
2026-04-16 15:40:40 +0530

    excel File Handeling

--- start/excelwork/writeexcel.py
+++ start/excelwork/writeexcel.py
@@ -1,18 +1,45 @@
-from openpyxl import Workbook
-
-filename = input("enter file name: ")
-filename += ".xlsx"
-#
-# myList = []
+from openpyxl import Workbook, load_workbook
 
 
+# # enter data in particular row
+# filename = input("enter file name: ")
 #
 # wb = Workbook()
-#
 # sheet = wb.active
-# sheet.title = "data"
 #
+# rowNum = int(input("enter row number: "))
+# data = input("enter value: ")
+# if rowNum <= 0:
+#     print("enter valid row number")
+# else:
+#     for i in range(5):
+#         sheet.cell(row=rowNum, column=i+1).value = data
 #
+# wb.save(f"{filename}.xlsx")
+
+
+# # enter data in particular column
+# filename = input("enter file name: ")
 #
-# wb.save(filename)
+# wb = Workbook()
+# sheet = wb.active
 #
+# data = input("enter value: ")
+#
+# for i in range(5):
+#     sheet.cell(row=i+1, column=5).value = data
+#
+# wb.save(f"{filename}.xlsx")
+
+
+#read perticular column value
+wb = load_workbook("column.xlsx")
+sheet = wb.active
+
+colname = input("enter column name: ")
+
+rows = sheet.max_row
+print(sheet[colname+"1"].value)
+for cell in sheet[colname]:
+    print(cell.value)
+
