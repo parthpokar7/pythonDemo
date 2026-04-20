c17621108d68e17a8ebebe3c315144767475660a
parthpokar7 <parthpokar.77@gmail.com>
2026-04-20 18:14:40 +0530

    refectoring code

--- start/operatorsWork/membershipOperators.py
+++ start/operatorsWork/membershipOperators.py
@@ -1,7 +1,7 @@
 data = ["parth", 26, "Ahmedabad", 9624209757]
 
-print ("parth" in data)
-print ("Parth" not in data)
+print("parth" in data)
+print("Parth" not in data)
 print(26 not in data)
 print(54 in data)
 print(54 not in data)
